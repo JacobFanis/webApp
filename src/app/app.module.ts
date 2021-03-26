f3dc7bf1f89739c79535832d5ecefb3a091afc32
Jacob Fanis <47834448+JacobFanis@users.noreply.github.com>
2021-03-26 10:59:31 -0500

    basic example
    upload test + project skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { NotFoundComponent } from './pages/not-found/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
